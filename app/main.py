74cd0f7686154f826abf4f21450a170eee6d5ffa
Abhiram1524 <masnaabhiram2002@gmail.com>
2024-06-12 20:14:21 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -8,6 +8,13 @@ def main():
 
     # Wait for user input
     input()
+    command=input()
+    """no_space=command.strip()
+    if no_space:
+        cmd=no_space.split()
+        cmd1=cmd[0]"""
+    sys.stdout.write(f"{command}: command not found\n")
+    
 
 
 if __name__ == "__main__":
