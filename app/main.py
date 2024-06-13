01521b5ee2d93bd38e6ab4a235610bc3fd071dd2
Abhiram1524 <masnaabhiram2002@gmail.com>
2024-06-13 22:52:26 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+commands={"exit","echo","type"}
 
 
 def main():
@@ -11,6 +12,11 @@ def main():
             sys.exit(0)
         elif cmd[0]=="echo":
             sys.stdout.write(" ".join(cmd[1:]) + "\n")
+        elif cmd[0]=="type":
+            if cmd[1] in commands:
+                sys.stdout.write(f"{cmd[1]} is a shell builtin")
+            else:
+                sys.stdout.write(f"{cmd[1]}: not found")
         else:
             sys.stdout.write(f"{command}: command not found\n")
         
