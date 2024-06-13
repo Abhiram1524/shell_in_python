43502987877b7c69788890489951d896ecf71c78
Abhiram1524 <masnaabhiram2002@gmail.com>
2024-06-13 19:46:07 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -2,18 +2,11 @@ import sys
 
 
 def main():
-    while true:
-    
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
-
-    # Wait for user input
-    command=input()
-    """no_space=command.strip()
-    if no_space:
-        cmd=no_space.split()
-        cmd1=cmd[0]"""
-    sys.stdout.write(f"{command}: command not found\n")
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        command=input()
+        sys.stdout.write(f"{command}: command not found\n")
     
 
 
