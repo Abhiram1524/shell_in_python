c8dc816db4062da6f828194a9706406d47e7e93f
Abhiram1524 <masnaabhiram2002@gmail.com>
2024-06-13 20:03:23 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -5,12 +5,12 @@ def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        command=input()
+        command=input().strip()
         cmd=command.split()
         if command=="exit 0":
             sys.exit(0)
         elif cmd[0]=="echo":
-            sys.stdout(" ".join(cmd[1:]))
+            sys.stdout.write(" ".join(cmd[1:]) + "\n")
         else:
             sys.stdout.write(f"{command}: command not found\n")
         
