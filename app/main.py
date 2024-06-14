042ae60363fd498bcf707d78485c13ab4bfb0abd
Abhiram1524 <masnaabhiram2002@gmail.com>
2024-06-14 20:41:57 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,36 +1,39 @@
 import sys
-commands={"exit","echo","type"}
+import os
 
+# Define a set of built-in commands
+commands = {"exit", "echo", "type"}
 
 def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        command=input().strip()
-        cmd=command.split()
-        if command=="exit 0":
+        command = input().strip()
+        cmd = command.split()
+        
+        if command == "exit 0":
             sys.exit(0)
-        elif cmd[0]=="echo":
+        elif cmd[0] == "echo":
             sys.stdout.write(" ".join(cmd[1:]) + "\n")
-        elif cmd[0]=="type":
-            if cmd[1] in commands:
-                sys.stdout.write(f"{cmd[1]} is a shell builtin\n")
+        elif cmd[0] == "type":
+            if len(cmd) > 1:
+                target_command = cmd[1]
+                if target_command in commands:
+                    sys.stdout.write(f"{target_command} is a shell builtin\n")
+                else:
+                    found = False
+                    for path_org in os.environ.get("PATH", "").split(":"):
+                        example = os.path.join(path_org, target_command)
+                        if os.access(example, os.X_OK):
+                            sys.stdout.write(f"{target_command} is {example}\n")
+                            found = True
+                            break
+                    if not found:
+                        sys.stdout.write(f"{target_command}: not found\n")
             else:
-                found=False
-                for path_org in os.environ.get("PATH","").split(":"):
-                    example=os.path.join(path_org,cmd[1])
-                    if os.access(example,os.X_OK):
-                        sys.stdout.write(f"{cmd[1]} is {example}\n")
-                        found=True
-                        break
-                    
-            else:
-                sys.stdout.write(f"{cmd[1]}: not found\n")
+                sys.stdout.write("type: usage: type command\n")
         else:
             sys.stdout.write(f"{command}: command not found\n")
-        
-    
-
 
 if __name__ == "__main__":
     main()
